6d08a783871156434cd9cc112533519c296296b3
Rajesh Kumar <rajeshsvm1@gmail.com>
2021-09-21 23:04:18 +0530

    Guess the number program added

---
+++ oops_class/guessnumber.py
@@ -0,0 +1,12 @@
+import random
+num=random.randint(1,100)
+# print(num)
+while True:
+    a=int(input("Enter the number: "))
+    if a==num:
+        print("You have guessed correct number")
+        break
+    elif a>(num):
+        print("Guessed number is high")
+    else:
+        print("Guessed number is lower")
